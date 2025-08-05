c72d7ce93b28e73c59459de10ce111a9abb283ab
Pugazh P <pugazh@gighz.com>
2025-08-05 18:53:50 +0530

    Modify the css

--- src/Feed.jsx
+++ src/Feed.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import Content from './Content'
+import Reel from './Reel'
 
 function Feed() {
   return (
     <div className='parent'>
-        <div className='Reel-page'>Reels</div>
+        <div className='Reel-page'><Reel/></div>
         <div className=''><Content/></div>
     </div>
   )
